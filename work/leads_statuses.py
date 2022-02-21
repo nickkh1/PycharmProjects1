f4a138d6f121b849a0b64df1f593a769201db805
nick- <nick-khrebtov@yandex.ru>
2022-02-21 18:50:56 +0300

    +

--- work/leads_statuses.py
+++ work/leads_statuses.py
@@ -4,15 +4,15 @@ import xlsxwriter
 
 pd.set_option('display.max_columns', None)
 
-df = pd.read_csv('C:/Users/nick-/Desktop/CI/4.Analytics section/Leads_and_deals/Leads_dec.csv', sep=',')
+df = pd.read_csv('C:/Users/nick-/Desktop/CI/4.Analytics section/Sales/raw_data_for_Py/Leads_jan_2022.csv', sep=',')
 
-# print(df.head())
+print(df[df['Email'] == 'skandask82@gmail.com'])
 
 # ищем дубликаты
-mask = df.duplicated(subset=['Id'])
-df_dup = df[mask]
-df_dup.sort_values(by='Id', ascending=False)
-print(df_dup.head(15))
+# mask = df.duplicated(subset=['Id'])
+# df_dup = df[mask]
+# df_dup.sort_values(by='Id', ascending=False)
+# print(df_dup.head(15))
 
 
 # print(df.groupby('Lead Status')['Id'].agg('nunique').sort_values(ascending=False))
