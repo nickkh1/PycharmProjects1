c71d8d404b2df7832268c842ee0cd97106b27477
nick- <nick-khrebtov@yandex.ru>
2022-03-01 19:00:40 +0300

    +

--- DS_skillfactory/MOD_18_EDA/EDA_2.py
+++ DS_skillfactory/MOD_18_EDA/EDA_2.py
@@ -2,9 +2,38 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statistics
 
 pd.set_option('display.max_columns', None)
 
 df = pd.read_csv('C:/Users/nick-/Documents/DS/projects/SF_tasks/wine_cleared.csv', sep=',')
-print(df.info())
-print(df['price'].min())
+# print(df.info())
+# count_price = len(df['price']) # количество записей цен
+# sorted_price = sorted(df['price']) # отсортированные цены
+#
+# if count_price % 2: # при нечетном кол-ве элементов выбираем средний по индексу
+#     median = sorted_price[round(0.5*(count_price-1))]
+# else: # при четном кол-ве элементов выбираем 2 средних по индексу и считаем между ними среднее арифметическое
+#     index = sorted(x), round(0.5 * count_price)
+#     median_ = 0.5 * (sorted_price[index-1] + sorted_price[index])
+# print(median)
+
+# print(df.corr())
+
+
+# визуализируем
+# import matplotlib.pyplot as plt # библиотека визуализации
+# from scipy import stats # библиотека для расчетов
+#
+# plt.subplot(1, 2, 1) # задаем сетку рисунка количество строк и столбцов
+# stats.probplot(df['points'], plot=plt) # qq plot
+#
+# plt.subplot(1, 2, 2) # располагаем второй рисунок рядом
+# plt.hist(df['points']) # гистограмма распределения признака
+#
+# plt.tight_layout() # чтобы графики не наезжали другу на друга, используем tight_layout
+#
+# plt.show() # просмотр графика
+
+
+print(df.corr(method='kendall'))
---
+++ work/icici_processing.py
@@ -0,0 +1,70 @@
+import numpy as np
+import string
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
+bank_sheet = pd.read_excel('C:/Users/nick-/Desktop/CI/1.Payments/icic/arch/DetailedStatement_2022_03_01.xlsx',skiprows=16)
+
+#создаем словарь имя столбца: число(признак) на который надо заменить пропуски
+values = {
+    'Withdrawal Amt (INR)': 0,
+    'Deposit Amt (INR)':0,
+    'Cheque. No./Ref. No.': '-'
+}
+bank_sheet = bank_sheet.fillna(values)
+
+
+# делаем преобразвоание (удление запятой), чтобы склеить два столбца в один
+def repalce_comma(text):
+    new_text = text.replace(',','')
+    return new_text
+
+bank_sheet = bank_sheet.astype({'Withdrawal Amt (INR)': str})
+bank_sheet = bank_sheet.astype({'Deposit Amt (INR)': str})
+bank_sheet = bank_sheet.astype({'Balance (INR)': str})
+
+bank_sheet['Withdrawal Amt (INR)'] = bank_sheet['Withdrawal Amt (INR)'].apply(repalce_comma)
+bank_sheet['Deposit Amt (INR)'] = bank_sheet['Deposit Amt (INR)'].apply(repalce_comma)
+bank_sheet['Balance (INR)'] = bank_sheet['Balance (INR)'].apply(repalce_comma)
+
+bank_sheet = bank_sheet.astype({'Withdrawal Amt (INR)': float})
+bank_sheet = bank_sheet.astype({'Deposit Amt (INR)': float})
+bank_sheet = bank_sheet.astype({'Balance (INR)': float})
+
+#удаляем нижнюю часть
+bank_sheet = bank_sheet.dropna(subset=['Value Date'],axis=0)
+
+#приводим дату в нужный формат
+bank_sheet['Value Date'] = pd.to_datetime(bank_sheet['Value Date'],dayfirst=True)
+bank_sheet['year'] = bank_sheet['Value Date'].dt.year
+bank_sheet['month'] = bank_sheet['Value Date'].dt.month
+bank_sheet['day'] = bank_sheet['Value Date'].dt.day
+# bank_sheet = bank_sheet.astype({'year': str})
+# bank_sheet = bank_sheet.astype({'month': str})
+# bank_sheet = bank_sheet.astype({'day': str})
+# bank_sheet = bank_sheet.astype({'Value Date': str})
+bank_sheet['New date'] = bank_sheet['day'].astype(str) + '/' + bank_sheet['month'].astype(str) + '/' + bank_sheet['year'].astype(str)
+
+
+#внизу есть футер с комментариями к операциям за период
+# bank_sheet = bank_sheet.astype({'S.N.': float})
+
+
+
+
+#создаем столбец как в нашем файле
+bank_sheet['Transaction Amount(INR)'] = bank_sheet['Withdrawal Amt (INR)'] + bank_sheet['Deposit Amt (INR)']
+
+#создаем столюец DR/CR
+bank_sheet['Cr/Dr'] = bank_sheet['Withdrawal Amt (INR)'].apply(lambda x: 'DR' if x>0 else 'CR')
+
+#делаем структуру для занесения в файл
+new_bank_sheet = bank_sheet.reindex(columns=['S.N.','Tran. Id','New date','Transaction Posted Date',
+'Cheque. No./Ref. No.','Transaction Remarks','Cr/Dr','Transaction Amount(INR)','Balance (INR)']
+)
+
+new_bank_sheet.to_excel('C:/Users/nick-/Desktop/CI/1.Payments/icic/arch/from_py/DetailedStatement_2022_03_01.xlsx', index= False)
+# print(bank_sheet.head())
+# print(bank_sheet.info())
+# print(new_bank_sheet.head())
