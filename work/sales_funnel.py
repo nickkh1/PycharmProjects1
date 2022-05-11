57541a9bf23456616d8b7ecd24d498502207b480
nick- <nick-khrebtov@yandex.ru>
2022-05-12 00:50:28 +0300

    +

--- work/sales_funnel.py
+++ work/sales_funnel.py
@@ -7,17 +7,19 @@ import plotly.express as px
 
 pd.set_option('display.max_columns', None)
 
-df = pd.read_csv('C:/Users/nick-/Desktop/CI/4.Analytics section/for_Python/Funnel/'
-                 'leads_join_deals_v2_from_01_03_22_to_05_05_22.csv', sep=',')
+df_douplicated = pd.read_csv('C:/Users/nick-/Desktop/CI/4.Analytics section/for_Python/Funnel/'
+                 'leads_join_deals_v2_02_05_22_to_08_05_22.csv', sep=',')
+
+# после SQL запроса дублируются некоторые лиды, видимо из-за одинаковых статусов в таблицах для выборки
+df = df_douplicated.drop_duplicates()
 
 df['is_lead'] = df['Id'].apply(lambda x: 1 if x is not None else 0)
 
 df_dropped = df.dropna(axis=1).drop(columns=['Id', 'Last Name', 'Lead Owner Name', 'Lead Owner', 'Is Converted?',
-                                             'Full Name', 'Modified Time', 'Created By'])
+                                             'Full Name', 'Modified Time', 'Created By', 'Lead Status'])
 df_dropped["Created Time"] = pd.to_datetime(df_dropped["Created Time"])
 df_dropped['month'] = df_dropped['Created Time'].dt.month
 df_dropped = df_dropped.drop(columns=['Created Time'])
-
 '''выбираем месяц'''
 df_series = df_dropped[df_dropped['month']==5].drop(columns=['month']).sum()
 
