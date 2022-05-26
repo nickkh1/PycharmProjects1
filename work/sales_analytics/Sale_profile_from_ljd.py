e74ad4e9e1dfde03fff03b105a56cb85d291c4cb
nick- <nick-khrebtov@yandex.ru>
2022-05-26 22:16:40 +0300

    +

--- work/sales_analytics/Sale_profile_from_ljd.py
+++ work/sales_analytics/Sale_profile_from_ljd.py
@@ -4,6 +4,7 @@ import pandas as pd
 import seaborn as sns
 pd.set_option('display.max_columns', None)
 
+
 deals_initial = pd.read_csv('C:/Users/nick-/Desktop/CI/4.Analytics section/for_Python/Student_analytics/leads_join_deals_v2.csv', sep=',')
 deals_reformat = deals_initial[['d.Id', 'l.Created Time', 'd.Stage', 'l.Course Name', 'd.Category',
                                 'd.Current salary', 'd.Education', 'd.Employment status', 'd.Goal for study', 'd.Lead quality',
@@ -33,11 +34,11 @@ leads_deals_reformat['work_hour'] = leads_deals_reformat['hour'].apply(lambda x:
 
 leads_deals_reformat = leads_deals_reformat[leads_deals_reformat['year'] == 2022]
 
-'''обрабатывваем конверсию по времени суток'''
-hour_cr = leads_deals_reformat.groupby(by=['work_hour'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
-hour_cr['proportion'] = round(hour_cr['sum'] / hour_cr['count']*100, 1)
-hour_cr = hour_cr.sort_values(by='work_hour', ascending=True)
-print(hour_cr)
+'''обрабатывваем конверсию по рабочему/нерабочему времени суток'''
+working_hour_cr = leads_deals_reformat.groupby(by=['work_hour'])['success'].agg(['sum', 'count']).reset_index()
+working_hour_cr['proportion'] = round(working_hour_cr['sum'] / working_hour_cr['count']*100, 1)
+working_hour_cr = working_hour_cr.sort_values(by='work_hour', ascending=True)
+print(working_hour_cr)
 
 # Set the figure size
 plt.figure(figsize=(12, 7))
@@ -46,15 +47,61 @@ plt.figure(figsize=(12, 7))
 ax = sns.barplot(
     x='work_hour',
     y="proportion",
+    data=working_hour_cr,
+    )
+
+ax.set_title('Hourly conversions by working time, %', size=20)
+
+'''делаем AB тест пропорций'''
+from statsmodels.stats.proportion import proportions_ztest
+
+# нулевая и альтернативная гипотезы
+H0 = 'Конверсии в группах А и B равны'
+H1 = 'Конверсия в группе А ниже, чем конверсия в группе B'
+alpha = 0.05 # уровень значимости
+# вычисляем значение p-value для z-теста для пропорций
+_, p_value = proportions_ztest(
+    count=working_hour_cr['sum'], #число "успехов"
+    nobs=working_hour_cr['count'], #общее число наблюдений
+    alternative='smaller',
+)
+# выводим результат на экран
+print('p-value: ', round(p_value, 2))
+# сравниваем полученное p-value с уровнем значимости
+if (p_value < alpha):
+    print("Отвергаем нулевую гипотезу. {}".format(H0))
+    print("Альтернативная гипотеза. H1: {}".format(H1))
+else:
+    print("Принимаем нулевую гипотезу. {}".format(H0))
+
+
+
+'''обрабатывваем конверсию по  времени суток'''
+hour_cr = leads_deals_reformat.groupby(by=['hour'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
+hour_cr['proportion'] = round(hour_cr['sum'] / hour_cr['count']*100, 1)
+hour_cr = hour_cr.sort_values(by='hour', ascending=True)
+print(hour_cr)
+
+# Set the figure size
+plt.figure(figsize=(12, 7))
+
+# plot a bar chart
+ax = sns.barplot(
+    x='hour',
+    y="proportion",
     data=hour_cr,
     )
 
 ax.set_title('Hourly conversions, %', size=20)
+
 plt.show()
 
 
 
-'''форматируем датасет deals'''
+'''
+
+
+#форматируем датасет deals
 list_was_success = ['Downpayment made (upfront)', 'Downpayment made', 'Internal EMI in progress', 'Closed Won']
 deals_reformat['success'] = deals_reformat['d.Stage'].apply(lambda x: 1 if x in list_was_success else 0)
 deals_reformat['l.Created Time'] = pd.to_datetime(deals_reformat['l.Created Time'])
@@ -70,7 +117,7 @@ deals_reformat = deals_reformat[deals_reformat['year'] == 2022]
 # print(deals_reformat.head())
 
 
-'''обрабатываем goal_for_study'''
+# обрабатываем goal_for_study
 goal_for_study = deals_reformat.groupby(by=['d.Goal for study'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
 goal_for_study['proportion'] = round(goal_for_study['sum'] / goal_for_study['count']*100,1)
 goal_for_study = goal_for_study.sort_values(by='sum', ascending=False)
@@ -109,7 +156,7 @@ ax.set_title('"Goal for study" conversion to success, %', size=20)
 
 
 
-'''обрабатываем Lead quality'''
+# обрабатываем Lead quality
 lead_qual = deals_reformat.groupby(by=['Lead quality'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
 lead_qual['proportion'] = round(lead_qual['sum'] / lead_qual['count']*100,1)
 lead_qual = lead_qual.sort_values(by='sum', ascending=False)
@@ -134,7 +181,7 @@ ax.set_title('"Lead Quality" conversion to success, %', size=20)
 
 
 
-''' обрабатываем Current salary'''
+# обрабатываем Current salary
 salary_category_dict = {'0': 'low', '2': 'low', '3-4': 'low', '5-6': 'high', '7-8': 'high', '8+': 'high'}
 deals_reformat_upd1 = deals_reformat.replace({"Current salary": salary_category_dict})
 cur_salary = deals_reformat_upd1.groupby(by=['Current salary'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
@@ -160,7 +207,7 @@ ax = sns.barplot(
 ax.set_title('"Current salary" conversion to success, %', size=20)
 
 
-''' обрабатываем Work Experience'''
+# обрабатываем Work Experience
 work_exp_dict = {'No experience': 'no', '1': 'low', '2-3': 'low', '4-5': 'high', '6-7': 'high', '8+': 'high'}
 deals_reformat_upd1 = deals_reformat.replace({"Work experience": work_exp_dict})
 work_exp = deals_reformat_upd1.groupby(by=['Work experience'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
@@ -184,7 +231,7 @@ ax = sns.barplot(
 ax.set_title('"Work experience" conversion to success, %', size=20)
 
 
-''' обрабатываем Education'''
+# обрабатываем Education
 education = deals_reformat.groupby(by=['Education'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
 education['proportion'] = round(education['sum'] / education['count']*100,1)
 education = education.sort_values(by='sum', ascending=False)
@@ -206,7 +253,7 @@ ax = sns.barplot(
 ax.set_title('"Education" conversion to success, %', size=20)
 
 
-''' обрабатываем Education'''
+# обрабатываем Education
 employment_status = deals_reformat.groupby(by=['Employment status'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
 employment_status['proportion'] = round(employment_status['sum'] / employment_status['count']*100,1)
 employment_status = employment_status.sort_values(by='sum', ascending=False)
@@ -230,7 +277,7 @@ ax.set_title('"Employment status" conversion to success, %', size=20)
 # plt.show()
 
 
-''' обрабатываем Online education experience'''
+# обрабатываем Online education experience
 online_ed_exp = deals_reformat.groupby(by=['Online education experience'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
 online_ed_exp['proportion'] = round(online_ed_exp['sum'] / online_ed_exp['count']*100,1)
 online_ed_exp = online_ed_exp.sort_values(by='sum', ascending=False)
@@ -254,7 +301,7 @@ ax.set_title('"Online education experience" conversion to success, %', size=20)
 
 
 
-''' обрабатываем Category'''
+# обрабатываем Category
 category = deals_reformat.groupby(by=['Category'], as_index=False)['success'].agg(['sum', 'count']).reset_index()
 category['proportion'] = round(category['sum'] / category['count']*100,1)
 category = category.sort_values(by='sum', ascending=False)
@@ -278,7 +325,7 @@ ax.set_title('"Category" conversion to success, %', size=20)
 # plt.show()
 
 
-'''строим общий рисунок'''
+# строим общий рисунок
 # sns.set(style="darkgrid")
 #
 # fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(14, 8),
@@ -345,8 +392,12 @@ ax.set_title('"Category" conversion to success, %', size=20)
 
 
 
-'''дальнейшие шаги исследования'''
+# дальнейшие шаги исследования
+
 # сделать тесты для пропорций, где можно
 # собрать данные по полу
 # собрать данные по взаимодействию признаков
 # подгрузить даты создания лида и собрать новые признаки - конверсии по созданию лида в течение дня, в течение недели
+
+
+'''
